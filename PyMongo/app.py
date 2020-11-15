fc367a78ed0cc5a3fc0ec7bd5ad02d8b47b37ab6
Cenz Wong <cenzth@gmail.com>
2020-11-15 14:47:45 +0000

    update

--- PyMongo/app.py
+++ PyMongo/app.py
@@ -6,3 +6,9 @@ client = MongoClient(URL_Mongo)
 # Accessing Database
 print(client.list_database_names())
 db_credit = client['credit']
+
+db_credit_installment = db_credit['installment']
+
+documents = db_credit_installment.find({})
+documents[0]["AMT_PAYMENT"]
+# >>3982.05
