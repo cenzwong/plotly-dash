055881b30ad1c6448fd71bdfde5b5d2f6adb0804
Cenz Wong <cenzth@gmail.com>
2020-11-15 09:32:50 +0000

    update

--- PyMongo/app.py
+++ PyMongo/app.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-URL_Mongo = 'mongodb://msbd5003-mongodb:zsETiWDaAJdqODfOg62miplS9kHMEs5N5xhmgyIzSzEaOKVfMdJp7IUThBqd7obK1OBsnR1n2hHqMdORMCCx6w==@msbd5003-mongodb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@msbd5003-mongodb@'
+URL_Mongo = ''
 client = MongoClient(URL_Mongo)
 
 # Accessing Database
