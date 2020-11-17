b99760a261267d8dfd13897d74100d6091ec5156
Cenz Wong <cenzth@gmail.com>
2020-11-17 09:32:51 +0000

    update

--- project/app.py
+++ project/app.py
@@ -3,15 +3,19 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
+# for all those graph: https://plotly.com/python/pie-charts/
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 
 #====================================
 from pymongo import MongoClient
 import pprint as pp
+
 
 URL_Mongo = ''
 client = MongoClient(URL_Mongo)
@@ -19,6 +23,8 @@ client = MongoClient(URL_Mongo)
 # Accessing Database
 print(client.list_database_names())
 db_credit = client['credit']
+print(db_credit.list_collection_names())
+db_credit_application = db_credit['application']
 #===========================================
 
 
@@ -28,12 +34,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "x": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "y": [4, 1, 2, 2, 4, 5],
-})
-fig = px.bar(df, x="x", y="y")
-
+# ======================
 df2 = pd.DataFrame({
     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
     "Amount": [4, 1, 2, 2, 4, 5],
@@ -41,11 +42,19 @@ df2 = pd.DataFrame({
 })
 fig2 = px.bar(df2, x="Fruit", y="Amount", color="City", barmode="group")
 
+# ======================
 df = px.data.gapminder().query("year == 2007").query("continent == 'Europe'")
 df.loc[df['pop'] < 2.e6, 'country'] = 'Other countries' # Represent only large countries
-fig = px.pie(df, values='pop', names='country', title='Population of European continent')
-fig.show()
+fig3 = px.pie(df, values='pop', names='country', title='Population of European continent')
 
+# ======================
+application_Male = db_credit_application.count_documents({"CODE_GENDER": "M"})
+application_Female = db_credit_application.count_documents({"CODE_GENDER": "F"})
+
+labels = ['Male','Female']
+values = [application_Male, application_Female]
+# values = [44776, 44778]
+fig4 = go.Figure(data=[go.Pie(labels=labels, values=values)])
 
 app.layout = html.Div(children=[
     html.H1(children='Home Credit Default Risk'),
@@ -55,15 +64,26 @@ app.layout = html.Div(children=[
     '''),
 
     dcc.Graph(
-        id='example-graph',
-        figure=fig
+        id='application_male_female',
+        figure=fig4
     ),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig2
+    ),
+
+    dcc.Graph(
+        id='example-graph2',
+        figure=fig3
+    ),
+
 
 ])
 
 if __name__ == '__main__':
     # app.run_server(debug=True, port=8050, host='0.0.0.0')
-    app.run_server(debug=True, port=8050)
+    app.run_server(debug=False, port=8050)
 
 
 
