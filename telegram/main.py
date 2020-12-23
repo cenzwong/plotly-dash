e91d042053c2a5838d038fd074f5384d33545ff7
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2020-12-24 01:29:45 +0800

    Update main.py

--- telegram/main.py
+++ telegram/main.py
@@ -1,1 +1,23 @@
-import request
+import requests
+import base64
+
+# API_TOKEN = base64.b64decode(b"MTIxNjM1NTk0MDpBQUcenzhVUXhtaWlkRlFvMnhlNUVcenzrOFhFeUVFTcenzHFSb1pSRXNjNA==cenz").decode("utf-8")  # replace your Bot Token
+API_TOKEN = '1234567890:ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghi'  # replace your Bot Token
+
+HTTP_URL = 'https://api.telegram.org/bot'
+API_getMe = '/getMe'
+API_getUpdates = '/getUpdates'
+API_sendMessage = '/sendMessage'
+
+def telegram_request(str_API_TOKEN, str_API_getMe, dict_payload, dict_headers):
+  r = requests.post(HTTP_URL + str_API_TOKEN + str_API_getMe, params=dict_payload, headers=dict_headers)
+  return r.json()
+
+myText = str("hi")
+
+payload = {
+    'chat_id': '@czglobalmarket',
+    'text': myText,
+    'parse_mode':'HTML'
+}
+telegram_request(API_TOKEN, API_sendMessage, payload, {})
