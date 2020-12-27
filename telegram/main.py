f5de7aed22019452d414c745ed94fce36f300910
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2020-12-27 23:07:41 +0800

    Update main.py

--- telegram/main.py
+++ telegram/main.py
@@ -10,16 +10,39 @@ HTTP_URL = 'https://api.telegram.org/bot'
 API_getMe = '/getMe'
 API_getUpdates = '/getUpdates'
 API_sendMessage = '/sendMessage'
+API_sendPhoto = '/sendPhoto' # https://core.telegram.org/bots/api#sendphoto
+
+newLine = str("\r\n")
 
 def telegram_request(str_API_TOKEN, str_API_getMe, dict_payload, dict_headers):
   r = requests.post(HTTP_URL + str_API_TOKEN + str_API_getMe, params=dict_payload, headers=dict_headers)
   return r.json()
 
-myText = str("hi")
+def telegram_sendPhotofromFile(str_API_TOKEN, str_API_getMe, dict_payload, dict_headers, str_path):
+  photo = {'photo': open(str_path, 'rb')}
+  r = requests.post(HTTP_URL + str_API_TOKEN + str_API_getMe, params=dict_payload, files=photo)
+  return r.json()
 
+
+# Example
+## Code for sending Texy
+myText = str("ThisText")+ newLine + str("ThatText")
 payload = {
-    'chat_id': '@czglobalmarket',
+    'chat_id': '@djrbownfosnsoxnoenwofjronsowndo',
     'text': myText,
     'parse_mode':'HTML'
 }
 telegram_request(API_TOKEN, API_sendMessage, payload, {})
+## Code for sending url photo
+payload = {
+    'chat_id': '@djrbownfosnsoxnoenwofjronsowndo',
+    'photo': "https://api.time.com/wp-content/uploads/2019/08/better-smartphone-photos.jpg",
+    # 'parse_mode':'HTML'
+}
+telegram_request(API_TOKEN, API_sendPhoto, payload, {})
+## Code for sending local photo
+fig.savefig("myFile.jpg")
+payload = {
+    'chat_id': '@djrbownfosnsoxnoenwofjronsowndo',
+}
+telegram_sendPhotofromFile(API_TOKEN, API_sendPhoto, payload, {}, "myFile.jpg")
